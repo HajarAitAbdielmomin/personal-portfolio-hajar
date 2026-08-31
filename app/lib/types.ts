78c464faee930c460fdb94052f8be5fabc10d06a
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-08-31 13:52:24 +0100

    add file for data types

---
+++ app/lib/types.ts
@@ -0,0 +1,10 @@
+export interface Project {
+    title: string;
+    description: string;
+    tags: string[];
+    screenshots: string[];
+    architecture: string[];
+    github: string;
+    live: string;
+    featured: boolean;
+}
