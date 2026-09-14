57b7ac25a1350f5b8a518625749eb3b7e6beb458
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-09-14 16:29:24 +0100

    move project data interface to dataType file

--- app/types/dataType.ts
+++ app/types/dataType.ts
@@ -23,7 +23,14 @@ export interface Education {
     isGraduated?: boolean;
     footerText?: string;
 }
-
+export interface Project {
+    title: string;
+    description: string;
+    tags: string[];
+    screenshots: string[];
+    architecture: string[];
+    github: string;
+}
 export interface Internship {
     id: string;
     position: string;
